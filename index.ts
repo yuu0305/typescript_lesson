899a5ff02294a24cbdfe7b95fde6aba6a815973e
yuu0305 <segyayuu8@gmail.com>
2023-11-20 17:50:17 +0900

    一旦コミット

--- index.ts
+++ index.ts
@@ -1,6 +1,18 @@
 let hello: string = "hello"
 console.log(hello);
 
+// function
 function add(a: number, b: number): number {
    return a + b
 }
+
+// obgect
+const person: {
+   name: string;
+   age: number;
+} = {
+   name: 'Jack',
+   age: 27
+}
+
+console.log(person)
