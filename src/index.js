cb7e2883f30d89ea77d2071377daf17acf55d5bb
yuu0305 <segyayuu8@gmail.com>
2023-11-20 17:50:43 +0900

    設定ファイル等

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// jsに変換して実行
+// $ npm run build
+// node dist/index.js
+// tsを直接実行
+// npx ts-node src/index.ts
+var hello = "hello";
+console.log(hello);
+// function
+function add(a, b) {
+    return a + b;
+}
+// obgect
+var person = {
+    name: 'Jack',
+    age: 27
+};
+// array
+var fruits = ['Apple', 'Banana', 'Grape'];
+// tuple型
+var book = ['boolean', 1500, false];
+console.log(person);
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+// jsに変換して実行
+// $ npm run build
+// node dist/index.js
+
+// tsを直接実行
+// npx ts-node src/index.ts
+
+let hello: string = "hello"
+console.log(hello);
+
+// function
+function add(a: number, b: number): number {
+   return a + b
+}
+
+// obgect
+const person: {
+   name: string;
+   age: number;
+} = {
+   name: 'Jack',
+   age: 27
+}
+
+// array
+const fruits: string[] = ['Apple', 'Banana', 'Grape']
+
+// tuple型
+const book: [string, number, boolean] = ['boolean', 1500, false]
+
+console.log(person)
+
+
